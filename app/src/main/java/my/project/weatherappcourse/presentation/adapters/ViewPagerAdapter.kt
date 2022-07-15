f81c32bc3a733c4ac54bed2a0e8ba32b71adc4c2
Erkesh <dedeeverkesh@gmail.com>
2022-07-15 23:48:22 +0300

    Finished part 9 (added WeatherAdapter for recyclerView in HoursFragment)

--- app/src/main/java/my/project/weatherappcourse/presentation/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/my/project/weatherappcourse/presentation/adapters/ViewPagerAdapter.kt
@@ -4,7 +4,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapter(fa: FragmentActivity, private val list: List<Fragment>) : FragmentStateAdapter(fa) {
+class ViewPagerAdapter(
+    fragmentActivity: FragmentActivity,
+    private val list: List<Fragment>,
+) : FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount(): Int {
         return list.size
 
--- app/src/main/java/my/project/weatherappcourse/presentation/fragments/HoursFragment.kt
+++ app/src/main/java/my/project/weatherappcourse/presentation/fragments/HoursFragment.kt
@@ -6,20 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import my.project.weatherappcourse.R
+import my.project.weatherappcourse.databinding.FragmentHoursBinding
 
 
 class HoursFragment : Fragment() {
 
+    private lateinit var binding: FragmentHoursBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+    ): View {
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun initRecyclerView() = with(binding) {
+
+    }
+
 
     companion object {
         @JvmStatic
         fun newInstance() = HoursFragment()
     }
+
 }
